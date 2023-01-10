6ea31c9ec18eed7171e1eed46c60449ac765328c
william-ellis <11599796+william-ellis@users.noreply.github.com>
2023-01-10 11:55:22 +0100

    ci: set release variables and write version into manifest when building

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,13 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [string]$Version
+)
+
 Remove-Item .\output -Force -Recurse -ErrorAction Ignore
 New-Item .\output -ItemType Directory | Out-Null
+
+Update-ModuleManifest UpliftTest.psd1 -ModuleVersion $Version
 
 Copy-Item UpliftTest.psd1 output
 Copy-Item UpliftTest.psm1 output
